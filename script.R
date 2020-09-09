857de269cc7a5a41b5a8a678d122610d0d4bba7f
Renan Chun <rechun@usp.br>
2020-09-09 01:24:03 -0300

    prmeira diferença

--- script.R
+++ script.R
@@ -2,6 +2,7 @@
 #falta explicar
 library(readr)
 library(ggplot2)
+
 data <- read_csv("data/co2_mm_mlo (1).csv",na = '-99.99', comment = "#") #substituo por NA Todos os dados com -99.99 
 
 data
@@ -11,16 +12,21 @@ p <- ggplot(data, aes(x=`decimal date`, y=average)) +
   ylab("Média") +
   ggtitle("Série temporal da média de CO2")
 p
-month <- c(12,3,6,2,3,7)
+
+
 meses <- c('Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro')
-meses[month]
-meses
 # verificando a sazonalidade por ano e podemos vericar que no meio da ano.
 p <- ggplot(data, aes(x=month, y=average, color=as.factor(year))) +
   geom_line() + 
   xlab("")+
-  ggtitle("Série temporal da média de  por ano")
+  ggtitle("Série temporal da média de  por ano") +
+  scale_x_continuous(labels = meses[month])
 
 p
+#Primeira diferença remove não estacionariedade
 
-
+p <- ggplot(data, aes(x=`decimal date`, y=c(0,diff(average,differences = 1)))) +
+  geom_line() + 
+  ylab("Média") +
+  ggtitle("Série temporal da média de CO2")
+p
