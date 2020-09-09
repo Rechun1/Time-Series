10f36baf5b9b7e2cf9b7a4b99dd0755ed0bfd574
Renan Chun <rechun@usp.br>
2020-09-08 21:53:12 -0300

    gráfico de sazionalidade

--- script.R
+++ script.R
@@ -2,13 +2,10 @@
 #falta explicar
 library(readr)
 library(ggplot2)
-data <- read_csv("data/co2_mm_mlo (1).csv",na = '-99.99', comment = "#")
+data <- read_csv("data/co2_mm_mlo (1).csv",na = '-99.99', comment = "#") #substituo por NA Todos os dados com -99.99 
 
 data
 p <- ggplot(data, aes(x=`decimal date`, y=average)) +
   geom_line() + 
   xlab("")
 p
-
-
-#exer 2
