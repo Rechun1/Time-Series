5394361471cbf1447ebee9f30912cdc9e2c73963
Renan Chun <rechun@usp.br>
2020-09-10 10:10:09 -0300

    finish item 2

--- script.R
+++ script.R
@@ -2,6 +2,7 @@
 #falta explicar
 library(readr)
 library(ggplot2)
+library(moments)
 library(xts)
 data <- read_csv("data/co2_mm_mlo (1).csv",na = '-99.99', comment = "#") #substituo por NA Todos os dados com -99.99 
 
@@ -131,6 +132,108 @@ ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE,
 }
 
  <- as.xts(x = data_ibovespa[, -1], order.by = data_ibovespa$Data)
+acf(df_ts,na.action = na.pass)
+ggplot.corr(data = df_ts, lag.max = 24, ci= 0.95, large.sample.size = FALSE, horizontal = TRUE)
+
+#item c
+mean(datatest$log)
+p <- ggplot(data = datatest, aes(x = log)) +
+  geom_histogram() + 
+  stat_function(
+    fun = dnorm, 
+    args = list(mean = mean(datatest$log, na.rm=TRUE), sd = sd(datatest$log, na.rm=TRUE)), 
+    col = 'red'
+  )
+p
+
+skewness(datatest$log, na.rm = TRUE)
+kurtosis(datatest$log, na.rm = TRUE)
+
+#item d
+
+lrtn=diff(sqrt(log(data_ibovespa$Indice)),differences = 1)
+p <- ggplot(data_ibovespa, aes(x=Data, y=c(0,lrtn))) +
+  geom_line() + 
+  ylab("Média") +
+  ggtitle("Série temporal da média de CO2")
+p
+
+ggplot.corr <- function(data, lag.max = 24, ci = 0.95, large.sample.size = TRUE, horizontal = TRUE,...) {
+  
+  require(ggplot2)
+  require(dplyr)
+  require(cowplot)
+  
+  if(horizontal == TRUE) {numofrow <- 1} else {numofrow <- 2}
+  
+  list.acf <- acf(data, lag.max = lag.max, type = "correlation", plot = FALSE, na.action = na.pass)
+  N <- as.numeric(list.acf$n.used)
+  df1 <- data.frame(lag = list.acf$lag, acf = list.acf$acf)
+  df1$lag.acf <- dplyr::lag(df1$acf, default = 0)
+  df1$lag.acf[2] <- 0
+  df1$lag.acf.cumsum <- cumsum((df1$lag.acf)^2)
+  df1$acfstd <- sqrt(1/N * (1 + 2 * df1$lag.acf.cumsum))
+  df1$acfstd[1] <- 0
+  df1 <- select(df1, lag, acf, acfstd)
+  
+  list.pacf <- acf(data, lag.max = lag.max, type = "partial", plot = FALSE, na.action = na.pass)
+  df2 <- data.frame(lag = list.pacf$lag,pacf = list.pacf$acf)
+  df2$pacfstd <- sqrt(1/N)
+  
+  if(large.sample.size == TRUE) {
+    plot.acf <- ggplot(data = df1, aes( x = lag, y = acf)) +
+      geom_area(aes(x = lag, y = qnorm((1+ci)/2)*acfstd), fill = "#B9CFE7") +
+      geom_area(aes(x = lag, y = -qnorm((1+ci)/2)*acfstd), fill = "#B9CFE7") +
+      geom_col(fill = "#4373B6", width = 0.7) +
+      scale_x_continuous(breaks = seq(0,max(df1$lag),6)) +
+      scale_y_continuous(name = element_blank(), 
+                         limits = c(min(df1$acf,df2$pacf),1)) +
+      ggtitle("ACF") +
+      theme_bw()
+    
+    plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
+      geom_area(aes(x = lag, y = qnorm((1+ci)/2)*pacfstd), fill = "#B9CFE7") +
+      geom_area(aes(x = lag, y = -qnorm((1+ci)/2)*pacfstd), fill = "#B9CFE7") +
+      geom_col(fill = "#4373B6", width = 0.7) +
+      scale_x_continuous(breaks = seq(0,max(df2$lag, na.rm = TRUE),6)) +
+      scale_y_continuous(name = element_blank(),
+                         limits = c(min(df1$acf,df2$pacf),1)) +
+      ggtitle("PACF") +
+      theme_bw()
+  }
+  else {
+    plot.acf <- ggplot(data = df1, aes( x = lag, y = acf)) +
+      geom_col(fill = "#4373B6", width = 0.7) +
+      geom_hline(yintercept = qnorm((1+ci)/2)/sqrt(N), 
+                 colour = "sandybrown",
+                 linetype = "dashed") + 
+      geom_hline(yintercept = - qnorm((1+ci)/2)/sqrt(N), 
+                 colour = "sandybrown",
+                 linetype = "dashed") + 
+      scale_x_continuous(breaks = seq(0,max(df1$lag),6)) +
+      scale_y_continuous(name = element_blank(), 
+                         limits = c(min(df1$acf,df2$pacf),1)) +
+      ggtitle("ACF") +
+      theme_bw()
+    
+    plot.pacf <- ggplot(data = df2, aes(x = lag, y = pacf)) +
+      geom_col(fill = "#4373B6", width = 0.7) +
+      geom_hline(yintercept = qnorm((1+ci)/2)/sqrt(N), 
+                 colour = "sandybrown",
+                 linetype = "dashed") + 
+      geom_hline(yintercept = - qnorm((1+ci)/2)/sqrt(N), 
+                 colour = "sandybrown",
+                 linetype = "dashed") + 
+      scale_x_continuous(breaks = seq(0,max(df2$lag, na.rm = TRUE),6)) +
+      scale_y_continuous(name = element_blank(),
+                         limits = c(min(df1$acf,df2$pacf),1)) +
+      ggtitle("PACF") +
+      theme_bw()
+  }
+  cowplot::plot_grid(plot.acf, plot.pacf, nrow = numofrow)
+}
+
+<- as.xts(x = data_ibovespa[, -1], order.by = data_ibovespa$Data)
 acf(df_ts,na.action = na.pass)
 ggplot.corr(data = df_ts, lag.max = 24, ci= 0.95, large.sample.size = FALSE, horizontal = TRUE)
 
